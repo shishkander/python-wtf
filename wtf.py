c4470b3746f5e72e02dec7d6948651e00111a72e
Andrii Shyshkalov <tandrii@chromium.org>
2016-06-13 18:18:31 +0200

    Improve filenames + get references to system path stuff before anyone mocks them.

--- wtf.py
+++ wtf.py
@@ -7,11 +7,22 @@ import threading
 import traceback
 
 
-_lock = threading.RLock()  # THE lock to mutate module structures.
+_RLock = threading.RLock
+_path_exists = os.path.exists
+_path_join = os.path.join
+_path_split = os.path.split
+_path_dirname = os.path.dirname
+_path_norm = os.path.normpath
+_makedirs = os.makedirs
+_basename = os.path.basename
+_open = open
+
+_lock = _RLock()  # THE lock to mutate module structures.
 _locks_and_files = {}
 _conf = {
     'path': '/tmp/wtf',
     'file_per_thread': False,
+    'shorten_filepath': True,
 }
 _first_time = True
 
@@ -25,6 +36,7 @@ def reconfigure(**kwargs):
 
 
 def log(fmt_or_str, *fmt_args, **kwargs):
+  conf = _new_conf(**kwargs)
   _, filename, lineno, _, _, _ = inspect.stack()[1]
   try:
     msg = fmt_or_str % fmt_args
@@ -38,10 +50,29 @@ def log(fmt_or_str, *fmt_args, **kwargs):
   prefix = '%s %+20s:%-4s %s-%s-%s%-30s' % (
       # For this kind of logging, date is not important actually.
       str(datetime.datetime.now())[len("2016-16-13 "):],
-      filename, lineno,
+      _format_filename(filename, conf), lineno,
       os.getpid(), thread.ident, 'D' if thread.daemon else 'N',
       '(%s)' % thread.name)
-  return _write('%+50s: %s\n' % (prefix, msg), _new_conf(**kwargs))
+  return _write('%+50s: %s\n' % (prefix, msg), conf)
+
+
+def _format_filename(fname, conf):
+  if not conf['shorten_filepath']:
+    return fname
+  # x.y.z is enough for everybody :)
+  dirs, filename = _path_split(_path_norm(fname))
+  res = [filename]
+  while dirs and len(res) < 3:
+    new_dirs, dir = _path_split(dirs)
+    if new_dirs == dirs:
+      break
+    if '-' in dir or dir in ('site-packages', 'dist-packages'):
+      break
+    dirs = new_dirs
+    res.append(dir)
+  res.reverse()
+  return _path_join(*res)
+
 
 
 def _new_conf(**kwargs):
@@ -66,30 +97,32 @@ def _maybe_init():
     if not _first_time:
       return
     try:
-      os.makedirs(_conf['path'])
+      _makedirs(_conf['path'])
     except OSError as e:
       if e.errno != errno.EEXIST:
         raise
-    if not os.path.exists(_conf['path']):
+    if not _path_exists(_conf['path']):
       raise Exception("failed to create directory: %s", _conf['path'])
     _first_time = False
 
 
 def _get_lock_and_file(conf):
   tid = threading.current_thread().ident
-  key = tid if conf['file_per_thread'] else 'process'
+  key = ('%s-%s' % (os.getpid(), tid) if conf['file_per_thread']
+         else ('process-%s' % os.getpid()))
   try:
     return _locks_and_files[key]
   except KeyError:
-    path_prefix = os.path.join(conf['path'],
-                               '%s_%s' % (sys.argv[0] or 'python', os.getpid()))
+    path_prefix = _path_join(
+        conf['path'],
+        '%s_%s' % (_basename(sys.argv[0] or 'python'), os.getpid()))
     with _lock:
       _maybe_init()
       try:
         return _locks_and_files[key]
       except KeyError:
         ret = _locks_and_files[key] = (
-            (threading.RLock(), '%s_%s.log' % (path_prefix, tid))
+            (_RLock(), '%s_%s.log' % (path_prefix, tid))
             if conf['file_per_thread'] else
             (_lock, path_prefix + '.log')
         )
@@ -99,5 +132,5 @@ def _get_lock_and_file(conf):
 def _write(msg, conf):
   lock, logfile_path = _get_lock_and_file(conf)
   with lock:
-    with open(logfile_path, 'a') as f:
+    with _open(logfile_path, 'a') as f:
       f.write(msg)
